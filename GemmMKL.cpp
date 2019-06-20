2a703a40802aa02e7b7fc0373ee64e544dde9761
aidevnn <47622664+aidevnn@users.noreply.github.com>
2019-06-20 17:12:49 +0300

    Update GemmMKL.cpp

--- GemmMKL.cpp
+++ GemmMKL.cpp
@@ -50,6 +50,16 @@ void displayMatrix(int m, int n, float* Mat, string name, bool isrounded = true)
 
 void MatMul0(int m, int n, int k, float* a, float* b, float* c) {
 	for (int i = 0; i < m; ++i) {
+		for (int j = 0; j < n; ++j) {
+			for (int k0 = 0; k0 < k; ++k0) {
+				c[i * n + j] += a[i * k + k0] * b[k0 * n + j];
+			}
+		}
+	}
+}
+
+void MatMul1(int m, int n, int k, float* a, float* b, float* c) {
+	for (int i = 0; i < m; ++i) {
 		for (int k0 = 0; k0 < k; ++k0) {
 			for (int j = 0; j < n; ++j)
 				c[i * n + j] += a[i * k + k0] * b[k0 * n + j];
@@ -57,7 +67,7 @@ void MatMul0(int m, int n, int k, float* a, float* b, float* c) {
 	}
 }
 
-void MatMul1(int m, int n, int k, float* a, float* b, float* c) {
+void MatMul2(int m, int n, int k, float* a, float* b, float* c) {
 	cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, 1.0f, a, k, b, n, 0.0f, c, n);
 }
 
@@ -67,6 +77,7 @@ void testGemm(int m, int n, int k) {
 	float* B = (float *) malloc(n * k * sizeof(float));
 	float* C0 = (float *) malloc(m * n * sizeof(float));
 	float* C1 = (float *) malloc(m * n * sizeof(float));
+	float* C2 = (float *) malloc(m * n * sizeof(float));
 
 	for (int i = 0; i < m * k; ++i)
 		A[i] = getRand();
@@ -76,25 +87,29 @@ void testGemm(int m, int n, int k) {
 
 	MatMul0(m, n, k, A, B, C0);
 	MatMul1(m, n, k, A, B, C1);
+	MatMul2(m, n, k, A, B, C2);
 	displayMatrix(m, k, A, "A");
 	displayMatrix(k, n, B, "B");
-	displayMatrix(m, n, C0, "C0");
-	displayMatrix(m, n, C1, "C1");
+	displayMatrix(m, n, C0, "A x B meth0");
+	displayMatrix(m, n, C1, "A x B meth1");
+	displayMatrix(m, n, C2, "A x B meth2");
 
 	free(A);
 	free(B);
 	free(C0);
 	free(C1);
+	free(C2);
 }
 
 void benchGemm(int m, int n, int k) {
 
-	cout << "Start Bench..." << endl;
+	printf("Start Bench M=%i N=%i and K=%i \n", m, n, k);
 
 	float* A = (float *) malloc(m * k * sizeof(float));
 	float* B = (float *) malloc(n * k * sizeof(float));
 	float* C0 = (float *) malloc(m * n * sizeof(float));
 	float* C1 = (float *) malloc(m * n * sizeof(float));
+	float* C2 = (float *) malloc(m * n * sizeof(float));
 
 	for (int i = 0; i < m * k; ++i)
 		A[i] = getRand(false);
@@ -104,13 +119,14 @@ void benchGemm(int m, int n, int k) {
 
 	for (int i = 0; i < 5; ++i) {
 		for (int j = 0; j < m * n; ++j)
-			C0[i] = C1[i] = 0.0f;
+			C0[i] = C1[i] = C2[i] = 0.0f;
 
 		MatMul0(m, n, k, A, B, C0);
 		MatMul1(m, n, k, A, B, C1);
+		MatMul2(m, n, k, A, B, C2);
 	}
 
-	cout << "Bench methode 1" << endl;
+	cout << "Bench methode 0" << endl;
 	for (int i = 0; i < 5; ++i) {
 		for (int j = 0; j < m * n; ++j)
 			C0[i] = 0.0f;
@@ -120,7 +136,7 @@ void benchGemm(int m, int n, int k) {
 		printf("TIME %8.2f ms \n", (round(TIME * 100000.0) * 0.01));
 	}
 
-	cout << "Bench methode 2" << endl;
+	cout << "Bench methode 1" << endl;
 	for (int i = 0; i < 5; ++i) {
 		for (int j = 0; j < m * n; ++j)
 			C1[i] = 0.0f;
@@ -130,8 +146,24 @@ void benchGemm(int m, int n, int k) {
 		printf("TIME %8.2f ms \n", (round(TIME * 100000.0) * 0.01));
 	}
 
+	cout << "Bench methode 2" << endl;
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < m * n; ++j)
+			C2[i] = 0.0f;
+
+		now = high_resolution_clock::now();
+		MatMul2(m, n, k, A, B, C2);
+		printf("TIME %8.2f ms \n", (round(TIME * 100000.0) * 0.01));
+	}
+
 	cout << "End." << endl;
 	cout << endl;
+
+	free(A);
+	free(B);
+	free(C0);
+	free(C1);
+	free(C2);
 }
 
 int main() {
